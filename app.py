f36764a03c2e954721b6063205089e4fde08e948
Amritpal Singh <amritsunner29@gmail.com>
2020-12-17 13:22:33 -0500

    Added some web pages to the project will work on sql implementations later today

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/arod')
+def arod():
+    return render_template('arod.html')
+
+@app.route('/altitude')
+def altitude():
+    return render_template('altitude.html')
+
+@app.route('/peds')
+def peds():
+    return render_template('peds.html')
+
+@app.route('/lookup')
+def lookupPlayer():
+    return render_template('lookup.html')
